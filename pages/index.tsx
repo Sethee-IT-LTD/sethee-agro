563f51c1357a0073d84627df4794d790091de13f
Tony Stark <afemiadom@icloud.com>
2023-01-02 14:22:35 -0600

    Added feature and sample product section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
+import Collection from "../components/Collection";
+import Feature from "../components/Feature";
 import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Collection />
+      <Feature />
     </>
   );
 }
